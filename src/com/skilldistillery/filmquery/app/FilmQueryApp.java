159d680d94fa4a0e1f7281b957cca4639ebd427f
Kim Nettleton <kim@Brucebs-MacBook-Pro.local>
2021-06-27 15:06:26 -0600

    final update FilmQueryProject

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -15,36 +15,27 @@ public class FilmQueryApp {
 
 	public static void main(String[] args) throws SQLException {
 		FilmQueryApp app = new FilmQueryApp();
-//    app.test();
 		app.launch();
 	}
 
-//  private void test() throws SQLException {
-//    Film film = db.findFilmById(1);
-//    System.out.println(film);
-//    Actor actor = db.findActorById(3);
-//    System.out.println(actor);
-//  }
-
 	private void displayMenu() {
-	System.out.println("**********************************");
-	System.out.println("*** Welcome to Quirky Queries! ***");
-	System.out.println("**********************************");
-	System.out.println("********* Do you want to: ********");
-	System.out.println("**********************************");
-	System.out.println("****** 1.Search Film by Id *******");
-	System.out.println("**********************************");
-	System.out.println("***** 2. Search Actor by Id ******");
-	System.out.println("**********************************");
-	System.out.println("** 3.Search for Film by Keyword **");
-	System.out.println("**********************************");
-	System.out.println("************ 4.Exit **************");
-	System.out.println("**********************************");
+		System.out.println("**********************************");
+		System.out.println("*** Welcome to Quirky Queries! ***");
+		System.out.println("**********************************");
+		System.out.println("********* Do you want to: ********");
+		System.out.println("**********************************");
+		System.out.println("****** 1.Search Film by Id *******");
+		System.out.println("**********************************");
+		System.out.println("***** 2. Search Actor by Id ******");
+		System.out.println("**********************************");
+		System.out.println("** 3.Search for Film by Keyword **");
+		System.out.println("**********************************");
+		System.out.println("************ 4.Exit **************");
+		System.out.println("**********************************");
 	}
-	
+
 	private void launch() throws SQLException {
 		Scanner input = new Scanner(System.in);
-
 
 		boolean notExit = true;
 		while (notExit) {
@@ -56,7 +47,7 @@ public class FilmQueryApp {
 				startActorIdInterface2(input);
 			} else if (choice == 3) {
 				startKeywordInterface3(input);
-			}else if(choice == 4){
+			} else if (choice == 4) {
 				System.out.println("Okay then BuhBye!");
 				System.exit(0);
 			} else {
@@ -66,66 +57,64 @@ public class FilmQueryApp {
 
 		input.close();
 	}
-	
 
 	private void startFilmIdInterface1(Scanner input) throws SQLException {
 		System.out.println("****************************");
-		System.out.println("*** Search Films by Id! ****");
+		System.out.println("*** Search Films by Id  ****");
 		System.out.println("****************************");
 		System.out.println("**** Start by entering *****");
 		System.out.println("********** Film Id *********");
 		System.out.println("****************************");
-		
+
 		int searchId = input.nextInt();
 		input.nextLine();
 		Film film = db.findFilmById(searchId);
-		
-		if(film != null) {
-		    System.out.println(film);
-		}else{
-			System.out.println("Not a valid film Id. Try again.");
+
+		if (film != null) {
+			System.out.println(film);
+		} else {
+			System.out.println("Not a valid Film Id. Try again.");
 		}
-		
 
 	}
+
 	private void startActorIdInterface2(Scanner input) throws SQLException {
+		System.out.println("******************************");
+		System.out.println("** Search Films by Actor Id **");
 		System.out.println("*****************************");
-		System.out.println("**Search Films by Actor Id!**");
-		System.out.println("****************************");
-		System.out.println("**** Start by entering *****");
-		System.out.println("********** Actor Id *********");
-		System.out.println("****************************");
-		
+		System.out.println("**** Start by entering ******");
+		System.out.println("********** Actor Id **********");
+		System.out.println("******************************");
+
 		int actorFilmId = input.nextInt();
 		input.nextLine();
-		
+
 		Actor actor = db.findActorById(actorFilmId);
-		if(actor != null) {
+		if (actor != null) {
 			System.out.println(actor);
-		}else{
+		} else {
 			System.out.println("Not a valid Actor Id. Try again.");
 		}
-		
-		
+
 	}
 
 	private void startKeywordInterface3(Scanner input) throws SQLException {
-		
+
 		System.out.println("********************************");
-		System.out.println("*** Search film by Keyword! ****");
+		System.out.println("*** Search film by Keyword  ****");
 		System.out.println("********************************");
 		System.out.println("****** Start by entering: *******");
 		System.out.println("********************************");
 		System.out.println("*********** Keyword *************");
 		System.out.println("********************************");
-		
+
 		input.nextLine();
 		String keyword = input.nextLine();
 		boolean validSearch = true;
 		List<Film> film = db.findFilmByKeyword(keyword);
-		if(film.size() != 0) {
+		if (film.size() != 0) {
 			System.out.println(film);
-		}else {
+		} else {
 			System.out.println("No films match that keyword. Try again.");
 		}
 
